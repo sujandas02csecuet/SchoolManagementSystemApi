1ee55cf4f6d20e326d2b7831e304b657518ae9c7
Sujan Das <55094301+sujandas02csecuet@users.noreply.github.com>
2023-08-26 15:13:32 +0600

    Update StudentController.cs

--- SchoolManagementSystemApi/Controllers/StudentController.cs
+++ SchoolManagementSystemApi/Controllers/StudentController.cs
@@ -76,11 +76,15 @@ namespace SchoolManagementSystemApi.Controllers
         // edit from web 1 
         // edit from web 2
         // edit from web 3
-
-
+        
         // edit from web 4
         // edit from web 5
         // edit from web 6
+
+        // edit from web 7
+        // edit from web 8
+        // edit from web 9
+        
        
     }
 }
